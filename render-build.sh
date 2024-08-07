d9db4e730a672dc9161de30038c65520295b1714
pavithra kamalakkannan <pavithradeevi@gmail.com>
2024-08-07 09:34:01 +0530

    Prepare for deployment on Render

--- render-build.sh
+++ render-build.sh
@@ -6,9 +6,12 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 apt-get update && apt-get install -y google-chrome-stable
 
-# Check if Chrome is installed and output its path
+# Output the installation path of Chrome
 echo "Chrome installation path:"
 which google-chrome
+
+# Output the Chrome version
+google-chrome --version
 
 # Install npm dependencies
 npm install
